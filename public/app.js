c0ceb27891f40ea6f4e6f48cfbe7fcb4e9013028
Amine L'ch <aminelch95@gmail.com>
2021-01-14 17:58:09 +0100

    initial set of files

---
+++ public/app.js
@@ -0,0 +1,15 @@
+// on attend la chargement du page
+window.onload = (event) => {
+    console.log('page is fully loaded');
+
+    //pour recupèrer la meteo d'une ville
+
+    /** IL FAUT :
+      1 savoir l'addresse IP de visiteur
+      2 contacter une API pour savoir la localisation de l'utilisateur
+      3 renvoyé la méteo
+     */
+
+
+
+};
